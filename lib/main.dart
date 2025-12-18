bac367afb8e51df9b8a101bb1a082cc320d48d4f
dietervdsvdsdieter-create <dietervdsvdsdieter@gmail.com>
2025-12-18 22:07:28 +0100

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(const Scherreke13App());
+}
+
+class Scherreke13App extends StatelessWidget {
+  const Scherreke13App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Scherre'ke 13",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.orange,
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}
